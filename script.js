59e535982535a5cbd1c97f100e3af79746451bff
Mochila157 <mtmxxt@gmail.com>
2025-07-03 20:20:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Aqui futuramente você pode conectar com Firebase ou outro sistema
+// Por enquanto, apenas exemplo fixo
+document.getElementById("senhaAtual").innerText = "P001";
+document.getElementById("senhaAnterior").innerText = "C004";
